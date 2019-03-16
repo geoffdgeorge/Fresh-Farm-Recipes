0b252e0150ab2c7e369b6643a91c7043c55c1713
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-03-16 18:10:27 -0500

    Cleaned up code

--- site.js
+++ site.js
@@ -32,52 +32,47 @@ $(document).ready(function() {
               
           }
       
-      //alert("Congrats - we are at line 29");
+        //alert("Congrats - we are at line 29");
         const recipeSearch = "https://api.edamam.com/search?q=" + veg1 + veg2 + veg3 + "&app_id=a653f161&app_key=ff7f8bea67da7c853af3604e42724627&from=0&to=5";
       
         // Begin building an object to contain our API call's query parameters
         // Set the API key
         $.ajax({
-          url: recipeSearch,
-          method: "GET"
+            url: recipeSearch,
+            method: "GET"
         }).then(function(response) {
       
-          // Creating elements to hold the stuff         //var pOne = $("<p>").text("Rating: " + rating);
-          for (let j=0; j < response.hits.length; j++){
-      
-          let recipeName = response.hits[j].recipe.label;
-          let recipeImg = response.hits[j].recipe.image;
-          let recipeURL = response.hits[j].recipe.url;
-      
-          const recipeDiv = $('<div class="card mb-4" style="width: 12rem;">');
-        //   const recipeImageWrapper1 = $('<div class="row no-gutters">');
-          //Directly append img html using .html();
-        //   const recipeImageWrapper2 = $('<div class="col-md-4">');
-          //Append this to recipeWrapper
-          const recipeTextWrapper1 = $('<div class="card-body text-center">');
-        //   const recipeTextWrapper2 = $('<div class="text-center" id="recipe-name">');
-      
-          const recipeTitle = $(`<h5 class="card-title">${recipeName}</h5>`);
-          const recipeImage = $(`<img src=${recipeImg} class='card-img-top'>`);
-          const recipeLink = $(`<a target="_blank" href=${recipeURL} class="btn btn-primary">Get Recipe</a>`);
-      
-      
-           // Appending the stuff
-           recipeDiv.append(recipeImage);
-           recipeDiv.append(recipeTextWrapper1);
-              recipeTextWrapper1.append(recipeTitle);
-           recipeDiv.append(recipeLink) 
+            // Creating elements to hold the stuff         //var pOne = $("<p>").text("Rating: " + rating);
+            for (let j=0; j < response.hits.length; j++){
+        
+            let recipeName = response.hits[j].recipe.label;
+            let recipeImg = response.hits[j].recipe.image;
+            let recipeURL = response.hits[j].recipe.url;
+        
+            const recipeDiv = $('<div class="card mb-4" style="width: 12rem;">');
+            const recipeTextWrapper1 = $('<div class="card-body text-center">');
+        
+            const recipeTitle = $(`<h5 class="card-title">${recipeName}</h5>`);
+            const recipeImage = $(`<img src=${recipeImg} class='card-img-top'>`);
+            const recipeLink = $(`<a target="_blank" href=${recipeURL} class="btn btn-primary">Get Recipe</a>`);
+        
+        
+            // Appending the stuff
+            recipeDiv.append(recipeImage);
+            recipeDiv.append(recipeTextWrapper1);
+                recipeTextWrapper1.append(recipeTitle);
+            recipeDiv.append(recipeLink) 
             
-    
-           
-           $("#recipe-display").append(recipeDiv);
+
+            
+            $("#recipe-display").append(recipeDiv);
           }     
       });
-      }
-$(document).on("click", "#ingredient-submit", displayRecipes);
+    }
+    
+    $(document).on("click", "#ingredient-submit", displayRecipes);
 
-/* Market API Call and Click Function */
-
+    /* Market API Call and Click Function */
     function marketQuery() {
         event.preventDefault();
         const zip = $('#zip-code-input').val();
